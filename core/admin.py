85811525344b6716c9165550843f21525fd5e278
venediktova-kate <venediktova.kat@gmail.com>
2023-06-12 16:10:18 +0700

    Create admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+from core.models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'last_name', 'first_name', 'email')
+    readonly_fields = ('last_login', 'date_joined')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Special dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+
+admin.site.unregister(Group)
