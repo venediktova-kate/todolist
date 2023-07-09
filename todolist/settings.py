5c0bc05ab7d4f7067df7b2d7f183efea15129fcc
venediktova-kate <venediktova.kat@gmail.com>
2023-07-10 01:01:31 +0700

    Commit

--- todolist/settings.py
+++ todolist/settings.py
@@ -38,8 +38,7 @@ ROOT_URLCONF = 'todolist.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
