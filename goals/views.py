33983933e8d0f133ccfcc624d95196d04691bd61
venediktova-kate <venediktova.kat@gmail.com>
2023-07-10 20:19:08 +0700

    Add Create Category

--- goals/views.py
+++ goals/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework import permissions
 
-# Create your views here.
+from goals.models import GoalCategory
+
+
+class GoalCategoryCreateView(CreateAPIView):
+    model = GoalCategory
+    permission_classes = [permissions.IsAuthenticated]
