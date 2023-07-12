37072e439fdf4fc965332b245f44df196526512d
venediktova-kate <venediktova.kat@gmail.com>
2023-07-12 18:30:30 +0700

    Add category creation

--- goals/serializers.py
+++ goals/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+
+from core.serializers import ProfileSerializer
 from goals.models import GoalCategory
 
 
@@ -9,3 +11,12 @@ class GoalCreateSerializer(serializers.ModelSerializer):
         model = GoalCategory
         read_only_fields = ("id", "created", "updated", "user")
         fields = "__all__"
+
+
+class GoalCategorySerializer(serializers.ModelSerializer):
+    user = ProfileSerializer(read_only=True)
+
+    class Meta:
+        model = GoalCategory
+        fields = "__all__"
+        read_only_fields = ("id", "created", "updated", "user")
--- goals/views.py
+++ goals/views.py
@@ -1,9 +1,22 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework import permissions
 
 from goals.models import GoalCategory
+from goals.serializers import GoalCreateSerializer, GoalCategorySerializer
 
 
 class GoalCategoryCreateView(CreateAPIView):
     model = GoalCategory
     permission_classes = [permissions.IsAuthenticated]
+    serializer_class = GoalCreateSerializer
+
+
+class GoalCategoryListView(ListAPIView):
+    model = GoalCategory
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = GoalCategorySerializer
+
+    def get_queryset(self):
+        return GoalCategory.objects.filter(
+            user=self.request.user, is_deleted=False
+        )
