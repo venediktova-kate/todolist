e8d22d45d26d9289cb33ef99065d8340d1e94879
venediktova-kate <venediktova.kat@gmail.com>
2023-07-14 01:55:07 +0700

    Fill admin panel: add Category, add Goal, create Comment

--- goals/admin.py
+++ goals/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from goals.models import GoalCategory, Goal, GoalComment
+
+
+@admin.register(GoalCategory)
+class GoalCategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'id')
+    readonly_fields = ('created', 'updated')
+    list_filter = ('is_deleted',)
+    search_fields = ('title',)
+
+
+@admin.register(Goal)
+class GoalAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'id', 'category')
+    readonly_fields = ('created', 'updated')
+    search_fields = ('title', 'description')
+    list_filter = ('status', 'priority')
+
+
+@admin.register(GoalComment)
+class GoalCommentAdmin(admin.ModelAdmin):
+    ...
