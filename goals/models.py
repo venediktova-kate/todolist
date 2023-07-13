0ac730426ceaa60db20031f023f3852cbac9ceb2
venediktova-kate <venediktova.kat@gmail.com>
2023-07-13 22:50:14 +0700

    Complete goal model with priorities and statuses

--- goals/models.py
+++ goals/models.py
@@ -31,13 +31,25 @@ class Goal(BaseMode):
         verbose_name = "Цель"
         verbose_name_plural = "Цели"
 
+    class Status(models.IntegerChoices):
+        to_do = 1, "К выполнению"
+        in_progress = 2, "В процессе"
+        done = 3, "Выполнено"
+        archived = 4, "Архив"
+
+    class Priority(models.IntegerChoices):
+        low = 1, "Низкий"
+        medium = 2, "Средний"
+        high = 3, "Высокий"
+        critical = 4, "Критический"
+
     title = models.CharField(verbose_name="Название", max_length=255)
     description = models.TextField(verbose_name="Описание", blank=True, max_length=255)
     category = models.ForeignKey(GoalCategory, verbose_name="Категория", on_delete=models.PROTECT, related_name='goals')
     due_date = models.DateField(verbose_name="Дедлайн", default=date.today, blank=True, null=True)
     user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
-    status = ...
-    priority = ...
+    status = models.PositiveSmallIntegerField(verbose_name="Статус", choices=Status.choices, default=Status.to_do)
+    priority = models.PositiveSmallIntegerField(verbose_name="Приоритет", choices=Status.choices, default=Priority.medium)
 
     def __str__(self) -> str:
         return self.title
