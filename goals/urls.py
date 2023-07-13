ecf67562a281be48fb3b615114e4c3f390c21cc1
venediktova-kate <venediktova.kat@gmail.com>
2023-07-14 01:58:29 +0700

    Add CategoryDetails

--- goals/urls.py
+++ goals/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from goals import views
-
+from goals.views import GoalCategoryDetailsView
 
 urlpatterns = [
     path("goal_category/create", views.GoalCategoryCreateView.as_view(), name='create_category'),
     path("goal_category/list", views.GoalCategoryListView.as_view(), name='list_category'),
+    path('goal_category/<int:id>', GoalCategoryDetailsView.as_view(), name='category_details'),
 ]
--- goals/views.py
+++ goals/views.py
@@ -1,7 +1,9 @@
+from django.db import transaction
 from rest_framework.generics import CreateAPIView, ListAPIView
-from rest_framework import permissions
+from rest_framework import permissions, filters, generics
 
-from goals.models import GoalCategory
+from goals.models import GoalCategory, Goal
+from goals.permissions import GoalCategoryPermission
 from goals.serializers import GoalCreateSerializer, GoalCategorySerializer
 
 
@@ -15,8 +17,24 @@ class GoalCategoryListView(ListAPIView):
     model = GoalCategory
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = GoalCategorySerializer
+    filter_backends = [filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ['title', 'created']
+    ordering = ['title']
+    search = ['title']
 
     def get_queryset(self):
         return GoalCategory.objects.filter(
             user=self.request.user, is_deleted=False
         )
+
+
+class GoalCategoryDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [GoalCategoryPermission]
+    serializer_class = GoalCategorySerializer
+    queryset = GoalCategory.objects.select_related('user').exclude(is_deleted=True)
+
+    def perform_destroy(self, instance: GoalCategory) -> None:
+        with transaction.atomic():
+            instance.is_deleted = True
+            instance.save(update_fields=['is_deleted'])
+            instance.goal_set.update(status=Goal.Status.archived)
