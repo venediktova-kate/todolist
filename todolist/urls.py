1f3c487bb8e418cd49c689be94cf474395aae2f4
venediktova-kate <venediktova.kat@gmail.com>
2023-07-14 00:44:13 +0700

    Shorten code

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,20 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from core.views import SignUpView, LoginView, ProfileView, UpdatePasswordView
 
-
-app_name = 'core'
-
-core_patterns = ([
-                     path('signup', SignUpView.as_view(), name='signup'),
-                     path('login', LoginView.as_view(), name='login'),
-                     path('profile', ProfileView.as_view(), name='profile'),
-                     path('update_password', UpdatePasswordView.as_view(), name='update_password')
-], 'core')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('core/', include(core_patterns)),
+    path('core/', include("core.urls", "core")),
     path('oauth/', include("social_django.urls", namespace="social")),
-    path("goals/", include("goals.urls")),
+    path("goals/", include("goals.urls", "goals")),
 ]
