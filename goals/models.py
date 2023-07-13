00ac49e5c3ab02327edb07f0fec373dc70c1459f
venediktova-kate <venediktova.kat@gmail.com>
2023-07-13 22:43:48 +0700

    Create model Goal

--- goals/models.py
+++ goals/models.py
@@ -1,10 +1,19 @@
+from datetime import date
+
 from django.db import models
-from django.utils import timezone
 
 from core.models import User
 
 
-class GoalCategory(models.Model):
+class BaseMode(models.Model):
+    class Meta:
+        abstract = True
+
+    created = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True)
+    updated = models.DateTimeField(verbose_name="Дата последнего обновления", auto_now=True)
+
+
+class GoalCategory(BaseMode):
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
@@ -12,11 +21,23 @@ class GoalCategory(models.Model):
     title = models.CharField(verbose_name="Название", max_length=255)
     user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
     is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
-    created = models.DateTimeField(verbose_name="Дата создания")
-    updated = models.DateTimeField(verbose_name="Дата последнего обновления")
 
-    def save(self, *args, **kwargs):
-        if not self.id:  # Когда объект только создается, у него еще нет id
-            self.created = timezone.now()  # проставляем дату создания
-        self.updated = timezone.now()  # проставляем дату обновления
-        return super().save(*args, **kwargs)
+    def __str__(self):
+        return self.title
+
+
+class Goal(BaseMode):
+    class Meta:
+        verbose_name = "Цель"
+        verbose_name_plural = "Цели"
+
+    title = models.CharField(verbose_name="Название", max_length=255)
+    description = models.TextField(verbose_name="Описание", blank=True, max_length=255)
+    category = models.ForeignKey(GoalCategory, verbose_name="Категория", on_delete=models.PROTECT, related_name='goals')
+    due_date = models.DateField(verbose_name="Дедлайн", default=date.today, blank=True, null=True)
+    user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
+    status = ...
+    priority = ...
+
+    def __str__(self) -> str:
+        return self.title
