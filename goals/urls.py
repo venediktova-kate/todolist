a347376fd57c1c4aab3bfb2c1d8083374162ac3d
venediktova-kate <venediktova.kat@gmail.com>
2023-07-15 00:21:49 +0700

    Add goal urls

--- goals/urls.py
+++ goals/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
 from goals import views
-from goals.views import GoalCategoryDetailsView
 
 urlpatterns = [
     path("goal_category/create", views.GoalCategoryCreateView.as_view(), name='create_category'),
     path("goal_category/list", views.GoalCategoryListView.as_view(), name='list_category'),
-    path('goal_category/<int:id>', GoalCategoryDetailsView.as_view(), name='category_details'),
+    path('goal_category/<int:id>', views.GoalCategoryDetailsView.as_view(), name='category_details'),
+
+    path("goal/create", views.GoalCreateView.as_view(), name='create_goal'),
+    path("goal/list", views.GoalListView.as_view(), name='list_goal'),
+    path('goal/<int:id>', views.GoalDetailsView.as_view(), name='goal_details'),
 ]
