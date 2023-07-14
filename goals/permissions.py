eda49e3ff8c799eb7eca461a960e6b00ae1ff607
venediktova-kate <venediktova.kat@gmail.com>
2023-07-15 00:22:37 +0700

    Add comment permission

--- goals/permissions.py
+++ goals/permissions.py
@@ -2,7 +2,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.generics import GenericAPIView
 
-from goals.models import GoalCategory, Goal
+from goals.models import GoalCategory, Goal, GoalComment
 
 
 class GoalCategoryPermission(IsAuthenticated):
@@ -14,4 +14,10 @@ class GoalCategoryPermission(IsAuthenticated):
 class GoalPermission(IsAuthenticated):
 
     def has_object_permission(self, request: Request, view: GenericAPIView, obj: Goal) -> bool:
+        return request.user == obj.user
+
+
+class GoalCommentPermission(IsAuthenticated):
+
+    def has_object_permission(self, request: Request, view: GenericAPIView, obj: GoalComment) -> bool:
         return request.user == obj.user
