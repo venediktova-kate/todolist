e4b08ec6f99997a2a513569f7235860ab562eba3
venediktova-kate <venediktova.kat@gmail.com>
2023-07-16 23:14:32 +0700

    Create models Board, BoardParticipant, add board field to GoalCategory

--- goals/models.py
+++ goals/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from core.models import User
 
 
-class BaseMode(models.Model):
+class BaseModel(models.Model):
     class Meta:
         abstract = True
 
@@ -13,11 +13,54 @@ class BaseMode(models.Model):
     updated = models.DateTimeField(verbose_name="Дата последнего обновления", auto_now=True)
 
 
-class GoalCategory(BaseMode):
+class Board(BaseModel):
+    class Meta:
+        verbose_name = "Доска"
+        verbose_name_plural = "Доски"
+
+    title = models.CharField(verbose_name="Название", max_length=255)
+    is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class BoardParticipant(BaseModel):
+    class Meta:
+        unique_together = ("board", "user")
+        verbose_name = "Участник"
+        verbose_name_plural = "Участники"
+
+    class Role(models.IntegerChoices):
+        owner = 1, "Владелец"
+        writer = 2, "Редактор"
+        reader = 3, "Читатель"
+
+    board = models.ForeignKey(
+        Board,
+        verbose_name="Доска",
+        on_delete=models.PROTECT,
+        related_name="participants",
+    )
+    user = models.ForeignKey(
+        User,
+        verbose_name="Пользователь",
+        on_delete=models.PROTECT,
+        related_name="participants",
+    )
+    role = models.PositiveSmallIntegerField(
+        verbose_name="Роль", choices=Role.choices, default=Role.owner
+    )
+
+
+class GoalCategory(BaseModel):
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
 
+    board = models.ForeignKey(
+        Board, verbose_name="Доска", on_delete=models.PROTECT, related_name="categories"
+    )
     title = models.CharField(verbose_name="Название", max_length=255)
     user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
     is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
@@ -26,7 +69,7 @@ class GoalCategory(BaseMode):
         return self.title
 
 
-class Goal(BaseMode):
+class Goal(BaseModel):
     class Meta:
         verbose_name = "Цель"
         verbose_name_plural = "Цели"
@@ -55,7 +98,7 @@ class Goal(BaseMode):
         return self.title
 
 
-class GoalComment(BaseMode):
+class GoalComment(BaseModel):
     class Meta:
         verbose_name = "Комментарий"
         verbose_name_plural = "Комментарии"
