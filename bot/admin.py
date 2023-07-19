0c10de32c313e228fe56ed44716c8ffd7bd5b811
venediktova-kate <venediktova.kat@gmail.com>
2023-07-19 19:34:08 +0700

    Fill admin panel

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from bot.models import TgUser
+
+
+@admin.register(TgUser)
+class TgUserAdmin(admin.ModelAdmin):
+    list_display = ('chat_id', 'user')
+    readonly_fields = ['verification_code']
+    search_fields = ['chat_id']
