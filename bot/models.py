692bf9c03a31d51123e5388bb8a32e9c9a83e730
venediktova-kate <venediktova.kat@gmail.com>
2023-07-22 01:19:44 +0700

    Add create and update verification code

--- bot/models.py
+++ bot/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.crypto import get_random_string
 
 from core.models import User
 
@@ -7,7 +8,19 @@ class TgUser(models.Model):
     chat_id = models.BigIntegerField(primary_key=True, editable=False, unique=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     username = models.CharField(max_length=255, null=True, blank=True)
-    verification_code = models.CharField(max_length=255, unique=True, blank=True)
+    verification_code = models.CharField(max_length=20, unique=True, blank=True)
 
     def __str__(self):
         return f"{self.__class__.__name__} ({self.chat_id})"
+
+    @staticmethod
+    def _generate_verification_code() -> str:
+        return get_random_string(20)
+
+    def update_verification_code(self) -> None:
+        self.verification_code = self._generate_verification_code()
+        self.save(update_fields=['verification_code'])
+
+    @property
+    def is_verified(self) -> bool:
+        return bool(self.user)
