3e372c1f631bbd5bfe2d3efd9095bcab765c3db7
venediktova-kate <venediktova.kat@gmail.com>
2023-07-25 20:13:12 +0700

    Distribute command call code by function

--- bot/management/commands/runbot.py
+++ bot/management/commands/runbot.py
@@ -12,7 +12,6 @@ class Command(BaseCommand):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.tg_client = TgClient()
-        self.offset = 0
 
     def handle(self, *args, **options):
         offset = 0
